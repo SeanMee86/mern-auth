2370614a73222db86c33f04e3dcf83e8fe97d949
SeanMee86 <sean.mee86@yahoo.com>
2020-03-08 09:42:15 -0700

    building front end, setting up redux

--- routes/api/users.js
+++ routes/api/users.js
@@ -67,7 +67,7 @@ router.post('/login', (req, res) => {
                         const payload = {
                             id: user.id,
                             name: user.name
-                        }
+                        };
 
                         jwt.sign(
                             payload,
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
 
-mongoose.connect(db, {useNewUrlParser: true})
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.log(error));
 
